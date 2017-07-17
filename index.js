0a1b53f63699801df45547989104c2618fa41284
ioanniswd <ioanniswd@gmail.com>
2017-07-17 13:33:10 +0300

    On branch full_deployment
     Changes to be committed:
    	modified:   index.js
    
    Moved getAlias(using branch) to the top, before changing folders since the .git folder is no longer
    copied.

--- index.js
+++ index.js
@@ -31,73 +31,77 @@ var localPath = 'localLambdas/';
 var invokeFolder = process.cwd();
 console.log('current directory: ', invokeFolder);
 
-getFunctionName(function(err, functionName) {
+getBranches(function(err, currentBranch, otherBranches) {
   if (err) {
     console.log(err);
   } else {
+    let alias = currentBranch;
 
-    localPath += functionName;
-    console.log('localPath: ', localPath);
-    process.chdir(homedir);
-    if (!fs.existsSync(localPath)) {
-      console.log('local path does not exist');
-      var dirs = localPath.split('/');
-      console.log('dirs: ', dirs);
-      dirs.forEach(function(dir) {
-        if (!fs.existsSync(dir)) {
-          console.log('making dir: ', dir);
-          fs.mkdirSync(dir);
-        }
-        process.chdir(dir);
-      });
-    } else {
-      console.log('local path exists');
-      process.chdir(localPath);
-    }
+    getFunctionName(function(err, functionName) {
+      if (err) {
+        console.log(err);
+      } else {
 
-    ncp(invokeFolder, process.cwd(), {
-      filter: returnNotGit
-    }, function(err, files) {
-      console.log('files: ', files);
-
-      console.log('Removing unnecessary modules...');
-
-      exec('npm prune', function(err, stdout, stderr) {
-        if (err) {
-          console.log('err:', err);
+        localPath += functionName;
+        console.log('localPath: ', localPath);
+        process.chdir(homedir);
+        if (!fs.existsSync(localPath)) {
+          console.log('local path does not exist');
+          var dirs = localPath.split('/');
+          console.log('dirs: ', dirs);
+          dirs.forEach(function(dir) {
+            if (!fs.existsSync(dir)) {
+              console.log('making dir: ', dir);
+              fs.mkdirSync(dir);
+            }
+            process.chdir(dir);
+          });
         } else {
-          console.log('stderr: ', stderr);
-          console.log('Installing missing modules...');
+          console.log('local path exists');
+          process.chdir(localPath);
+        }
 
-          exec('npm-install-missing', function(err, stdout, stderr) {
-            console.log('err:', err);
-            console.log('stderr: ', stderr);
-            console.log(stdout);
-            console.log('Executing zip...');
-            console.log('pwd: ', process.cwd());
+        ncp(invokeFolder, process.cwd(), {
+          filter: returnNotGit
+        }, function(err, files) {
+          console.log('files: ', files);
 
-            exec(`zip -FSr ${functionName}.zip .`, {maxBuffer: 1024 * 1024}, function(err, stdout, stderr) {
-              if (err) {
-                console.log(err);
-              } else if (stderr) {
+          console.log('Removing unnecessary modules...');
+
+          exec('npm prune', function(err, stdout, stderr) {
+            if (err) {
+              console.log('err:', err);
+            } else {
+              console.log('stderr: ', stderr);
+              console.log('Installing missing modules...');
+
+              exec('npm-install-missing', function(err, stdout, stderr) {
+                console.log('err:', err);
                 console.log('stderr: ', stderr);
-              } else {
                 console.log(stdout);
-                console.log('Zip done.');
-                exec(`aws lambda  update-function-code --function-name ${functionName}  --zip-file fileb://${functionName}.zip`, {maxBuffer: 1024 * 1024}, function(err, stdout, stderr) {
+                console.log('Executing zip...');
+                console.log('pwd: ', process.cwd());
+
+                exec(`zip -FSr ${functionName}.zip .`, {
+                  maxBuffer: 1024 * 1024
+                }, function(err, stdout, stderr) {
                   if (err) {
                     console.log(err);
                   } else if (stderr) {
-                    console.log('stderr ', stderr);
+                    console.log('stderr: ', stderr);
                   } else {
-                    console.log('Upload done.');
-                    if (args.version || args.v) {
-
-                      getBranches(function(err, currentBranch, otherBranches) {
-                        if (err) {
-                          console.log(err);
-                        } else {
-                          let alias = currentBranch;
+                    console.log(stdout);
+                    console.log('Zip done.');
+                    exec(`aws lambda  update-function-code --function-name ${functionName}  --zip-file fileb://${functionName}.zip`, {
+                      maxBuffer: 1024 * 1024
+                    }, function(err, stdout, stderr) {
+                      if (err) {
+                        console.log(err);
+                      } else if (stderr) {
+                        console.log('stderr ', stderr);
+                      } else {
+                        console.log('Upload done.');
+                        if (args.version || args.v) {
 
                           // publish new version (keep version number)
                           publishVersion(functionName, function(err, version) {
@@ -120,7 +124,7 @@ getFunctionName(function(err, functionName) {
                                       console.log(err);
                                     } else {
                                       console.log('Current Branch/Lambda Alias:', alias);
-                                      if(otherBranches.length > 0) {
+                                      if (otherBranches.length > 0) {
                                         console.log('Other Branches:');
                                         otherBranches.forEach(function(branchName) {
                                           console.log(branchName);
@@ -138,20 +142,21 @@ getFunctionName(function(err, functionName) {
                               });
                             }
                           });
+
+                        } else {
+                          if (args.logs) {
+                            exec(`awslogs get /aws/lambda/${functionName} --watch`).stdout.pipe(process.stdout);
+                          }
                         }
-                      });
-                    } else {
-                      if (args.logs) {
-                        exec(`awslogs get /aws/lambda/${functionName} --watch`).stdout.pipe(process.stdout);
                       }
-                    }
+                    });
                   }
                 });
-              }
-            });
+              });
+            }
           });
-        }
-      });
+        });
+      }
     });
   }
 });
